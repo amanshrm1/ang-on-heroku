f7c61000f2ab73f3399d15cb07a4da5716112884
aman sharma1 <am00ansh@gmail.com>
2020-11-19 23:37:20 +0530

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = required('path');
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/e-comm'));
 
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname +
-    '/dist/e-comm/index.html'));
+    'dist/e-comm/index.html'));
 })
 
 app.listen(process.env.PORT || 8080)
